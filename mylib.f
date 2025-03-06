138f9a3dc720b5331bbac61b9d0a6e142b1b1ded
Abdullah Shams <shams3049@gmail.com>
2025-03-06 11:35:20 +0100

    Update mylib.f

--- mylib.f
+++ mylib.f
@@ -1,11 +1,9 @@
 module mymodule
-    use, intrinsic :: iso_c_binding
-    implicit none
 contains
-    subroutine add_numbers(a, b, result) bind(C, name="add_numbers")
-        use iso_c_binding
-        real(c_float), intent(in) :: a, b
-        real(c_float), intent(out) :: result
-        result = a + b
-    end subroutine add_numbers
+  subroutine add(a, b, result)
+    implicit none
+    integer, intent(in) :: a, b
+    integer, intent(out) :: result
+    result = a + b
+  end subroutine add
 end module mymodule
